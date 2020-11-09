b01339d11e7bd39686944022a2cda6469a15f1b0
lizardlove <1026123335@qq.com>
2020-11-09 10:52:41 +0800

    feat: change cnn model

--- Algorithms/models/model.py
+++ Algorithms/models/model.py
@@ -3,31 +3,51 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(1, 16, 2, 1)
+#         self.conv2 = nn.Conv2d(16, 32, 2, 1)
+#         self.dropout1 = nn.Dropout(0.25)
+#         self.dropout2 = nn.Dropout(0.5)
+#         self.fc1 = nn.Linear(18432, 128)
+#         self.fc2 = nn.Linear(128, 10)
+#
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = nn.ReLU()(x)
+#         x = nn.MaxPool2d(2, 1)(x)
+#         x = self.dropout1(x)
+#         x = self.conv2(x)
+#         x = nn.ReLU()(x)
+#         x = nn.MaxPool2d(2, 1)(x)
+#         x = self.dropout2(x)
+#         x = torch.flatten(x, 1)
+#         x = self.fc1(x)
+#         x = nn.ReLU()(x)
+#         x = self.fc2(x)
+#         output = F.log_softmax(x, dim=1)
+#         return output
+
 class Net(nn.Module):
+
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 2, 1)
-        self.conv2 = nn.Conv2d(16, 32, 2, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(18432, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = nn.ReLU()(x)
-        x = nn.MaxPool2d(2, 1)(x)
-        x = self.dropout1(x)
-        x = self.conv2(x)
-        x = nn.ReLU()(x)
-        x = nn.MaxPool2d(2, 1)(x)
-        x = self.dropout2(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = nn.ReLU()(x)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+        return F.log_softmax(x, dim=1)
+
 
 class Mclr_Logistic(nn.Module):
     def __init__(self, input_dim = 784, output_dim = 10):
